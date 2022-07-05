96f38ba626a75c51482598c2e6251c00022f7c79
alikz <ali6316k@gmail.com>
2022-07-05 20:49:21 +0430

    create marquee and elecatedbtn without style

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_class_a/ui_helper/theme_swicher.dart';
+import 'package:marquee/marquee.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../ui_helper/home_page_view.dart';
@@ -30,6 +31,7 @@ class HomePage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
+              // * top banner
               Padding(
                 padding: EdgeInsets.only(
                   top: 10,
@@ -39,31 +41,60 @@ class HomePage extends StatelessWidget {
                 child: SizedBox(
                   height: 160,
                   width: double.infinity,
-                  child: Stack(children: [
-                    HomePageView(
-                      controller: pageController,
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: SmoothPageIndicator(
+                  child: Stack(
+                    children: [
+                      HomePageView(
                         controller: pageController,
-                        count: 4,
-                        effect: ExpandingDotsEffect(
-                          dotWidth: 10,
-                          dotHeight: 10,
-                        ),
-                        onDotClicked: (index) {
-                          pageController.animateToPage(
-                            index,
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.easeInOut,
-                          );
-                        },
                       ),
-                    ),
-                  ]),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: SmoothPageIndicator(
+                          controller: pageController,
+                          count: 4,
+                          effect: ExpandingDotsEffect(
+                            dotWidth: 10,
+                            dotHeight: 10,
+                          ),
+                          onDotClicked: (index) {
+                            pageController.animateToPage(
+                              index,
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.easeInOut,
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              )
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 30,
+                child: Marquee(
+                  text: '🔊 this is place for news in application ',
+                  style: textTheme.bodySmall,
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Buy'),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Buy'),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
